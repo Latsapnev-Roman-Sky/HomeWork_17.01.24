444c17e9c973cca7de67ace31b1e70d87f285575
latsa <latsapnev01@mail.ru>
2024-01-16 20:45:58 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Task 1
+        int year = 2024;
+        checkLeapYear(year);
+    }
+    public static void checkLeapYear (int year) {
+        if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
